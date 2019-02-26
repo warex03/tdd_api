37cc89a7bbd7d1c8d0db8154ed2479c192b42d9a
Arvin Dino <warex03@gmail.com>
2019-02-27 03:23:05 +0800

    ✨ Add article seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create([
+  { title: 'Article 1', content: 'Article content 1', slug: 'article-1' },
+  { title: 'Article 2', content: 'Article content 2', slug: 'article-2' },
+  { title: 'Article 3', content: 'Article content 3', slug: 'article-3' },
+])
