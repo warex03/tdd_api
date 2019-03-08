b94c8f24eb7edb667b60b1565ccf3d01a4e1310a
Arvin Dino <warex03@gmail.com>
2019-03-09 00:41:30 +0800

    ✨ Update AccessTokenController controller and tests

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,2 +1,8 @@
 class AccessTokensController < ApplicationController
+  def create
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
+
+    render json: authenticator.access_token, status: :created
+  end
 end
