dd64143217ac0820d0c5d045e0e457f321823595
Arvin Dino <warex03@gmail.com>
2019-03-09 00:40:57 +0800

    ✨ Add AccessToken serializer and update tests

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -5,7 +5,7 @@ class AccessToken < ApplicationRecord
   private
 
   def generate_token
-    return if token.present? && AccessToken.exists?(token: token)
+    return if token.present? && !AccessToken.where.not(id: id).exists?(token: token)
     self.token = SecureRandom.hex(10)
   end
 end
